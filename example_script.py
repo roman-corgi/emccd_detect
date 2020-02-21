6919c4c9fcb497f8e90ba6a1946f04660d85cbf8
samhmiller <millerhsam@gmail.com>
2020-02-21 15:38:15 -0500

    Simplified example_script, renamed previous example_script photon_counting_script

--- example_script.py
+++ example_script.py
@@ -1,32 +1,27 @@
 # -*- coding: utf-8 -*-
-"""Gain and Photon Counting Simulation.
+"""EMCCD Detector Simulation.
 
-S Miller and B Nemati - UAH - 11-Feb-2020
+S Miller and B Nemati - UAH - 21-Feb-2020
 """
 import os
 
-import matplotlib.pyplot as plt
-import numpy as np
 from astropy.io import fits
 
 from emccd_detect.emccd_detect import emccd_detect
-from emccd_detect.photon_count import photon_count
 from emccd_detect.util.imagesc import imagesc
 
-
-plt.close('all')
 
 # Input fluxmap
 current_path = os.path.dirname(os.path.abspath(__file__))
 fits_name = 'ref_frame.fits'
-fluxmap = fits.getdata(os.path.join(current_path, 'emccd_detect', 'fits',
-                       fits_name))
-imagesc(fluxmap, 'Input Fluxmap')
+fits_path = os.path.join(current_path, 'emccd_detect', 'fits', fits_name)
+fluxmap = fits.getdata(fits_path)
 
+plot_images = True
 
-# Simulate frames with multiple different gains
 # Simulation inputs
 frametime = 100.0  # seconds
+gain = 1000.0
 cr_rate = 0  # hits/cm^2/s (set to 0 for no cosmics; 5 for L2 expected)
 bias = 0.0
 qe = 0.9  # quantum efficiency
@@ -36,47 +31,14 @@ dark_current = 0.005  # e-/pix/s
 cic = 0.02  # e-/pix/frame
 read_noise = 100  # e-/pix/frame -- amplifier noise (EMCCD CCD201 Type C)
 
-len_array = 5
-gain_array = np.logspace(0, 4, len_array, base=10)
-for gain in gain_array:
-    sim_im_gain = emccd_detect(fluxmap, cr_rate, frametime, gain, bias, qe,
-                               fwc_im, fwc_gr, dark_current, cic, read_noise)
-    imagesc(sim_im_gain,
+# Simulate single image
+sim_im = emccd_detect(fluxmap, cr_rate, frametime, gain, bias, qe, fwc_im,
+                      fwc_gr, dark_current, cic, read_noise)
+
+# Plot image
+if plot_images:
+    imagesc(fluxmap, 'Input Fluxmap')
+    imagesc(sim_im, 'Output Fluxmap\n'
             'Gain: {:.0f}    RN: {:.0f}e-    t_fr: {:.0f}s'.format(gain,
                                                                    read_noise,
                                                                    frametime))
-
-
-# Simulate photon counting
-gain = 1000.0
-sim_im = emccd_detect(fluxmap, cr_rate, frametime, gain, bias, qe, fwc_im,
-                      fwc_gr, dark_current, cic, read_noise)
-imagesc(sim_im, 'Analog', cmap='gray')
-
-pc_thresh = 5  # Photon counting threshold (to be multiplied by readnoise)
-sim_in_pc = photon_count(sim_im, read_noise, pc_thresh)
-imagesc(sim_in_pc, 'Photon Counted', cmap='gray')
-
-
-# Simulate co-added photon counted frames with short frametime
-# For best performance, choose frametime such that average e/pix/frame in
-# the region of interest (ROI) is roughly 0.1
-frametime_short = 1.0
-gain_short = 5000.0
-pc_thresh_short = 5
-
-n_frames = 100
-sim_ims = np.zeros((fluxmap.shape[0], fluxmap.shape[1], n_frames))
-sim_ims_pc = np.zeros(sim_ims.shape)
-for i in range(n_frames):
-    sim_ims[:, :, i] = emccd_detect(fluxmap, cr_rate, frametime_short,
-                                    gain_short, bias, qe, fwc_im, fwc_gr,
-                                    dark_current, cic, read_noise)
-    sim_ims_pc[:, :, i] = photon_count(sim_ims[:, :, i], read_noise,
-                                       pc_thresh_short)
-
-co_added = np.sum(sim_ims/gain_short, axis=2)
-co_added_pc = np.sum(sim_ims_pc, axis=2)
-
-imagesc(co_added, 'Co-Added Analog', vmax=100, cmap='gray')
-imagesc(co_added_pc, 'Co-Added Photon Counted', vmax=100, cmap='gray')
