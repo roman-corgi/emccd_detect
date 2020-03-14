c89a17a25496ed5e5fa671f008ead90293c13f1c
samhmiller <millerhsam@gmail.com>
2020-03-14 19:19:24 -0400

    Update pymat_diff.py

--- pymat_diff.py
+++ pymat_diff.py
@@ -3,11 +3,59 @@ import os
 
 from pathlib import Path
 
-current_path = Path(os.path.dirname(__file__))
-py_dir = Path(current_path, 'emccd_detect')
-mat_dir = Path(current_path, 'emccd_detect_matlab')
 
-py_exclude = ['config']
-py_list = sorted([f.stem for f in py_dir.rglob('[!__]*.py')
-                  if f.stem not in py_exclude])
-mat_list = sorted([f.stem for f in mat_dir.rglob('*.m')])
+class PymatDiffException(Exception):
+    """Exception class for pymat_diff module."""
+
+
+def get_filenames(path, ext, exclude=None):
+    """Get list of all filenames of a given extension in a directory.
+
+    Parameters
+    ----------
+    path : str
+        Full path of directory.
+    ext : str
+        Extension of files to be searched.
+    exclude : list, optional
+        List of names to exclude. Defaults to None.
+
+    Returns
+    -------
+    out : list
+        List of filenames stripped of extensions.
+
+    """
+    path = Path(path)
+    if not path.is_dir():
+        raise PymatDiffException('No such directory: {:}'.format(path))
+
+    if not exclude:
+        exclude = []
+
+    return sorted([f.stem for f in path.rglob('*.' + ext)
+                   if f.stem not in exclude])
+
+
+if __name__ == '__main__':
+    current_path = Path(os.path.dirname(__file__))
+    py_dir = Path(current_path, 'emccd_detect')
+    mat_dir = Path(current_path, 'emccd_detect_m')
+
+    py_exclude = ['__init__', 'config', 'imagesc']
+    mat_exclude = ['autoArrangeFigures', 'histbn']
+    py_list = get_filenames(py_dir, 'py', py_exclude)
+    mat_list = get_filenames(mat_dir, 'm', mat_exclude)
+
+    py_diff = [f for f in set(py_list) if f not in set(mat_list)]
+    mat_diff = [f for f in set(mat_list) if f not in set(py_list)]
+
+    if py_diff:
+        print('Python directory contains extra files not in Matlab:\n')
+        for name in py_diff:
+            print('   + ' + name)
+    print('\n')
+    if mat_diff:
+        print('Matlab directory contains extra files not in Python:\n')
+        for name in mat_diff:
+            print('   + ' + name)
