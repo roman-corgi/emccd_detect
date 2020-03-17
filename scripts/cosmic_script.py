fcd48fb47e0a9383a1d192942c44d8396cb4090f
samhmiller <millerhsam@gmail.com>
2020-03-17 18:05:25 -0400

    Update cosmic_script.py

--- scripts/cosmic_script.py
+++ scripts/cosmic_script.py
@@ -5,5 +5,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from emccd_detect.cosmic_hits import cosmic_hits
+from emccd_detect.util.imagesc import imagesc
 
-# Input fluxmap
+framesize = 1000
+image_frame = np.ones(framesize)
+cr_rate = 5.  # Cosmic ray rate (5 for L2) (hits/cm^2/s)
+frametime = 100.  # Frame time (s)
+pixel_pitch = 13e-6  # Distance between pixel centers (m)
+full_well_image = 60000.  # Image area full well capacity (e-)
+
+hits_frame = cosmic_hits(image_frame, cr_rate, frametime, pixel_pitch,
+                         full_well_image)
+
+imagesc(hits_frame, 'Cosmic Hits')
+plt.show()
