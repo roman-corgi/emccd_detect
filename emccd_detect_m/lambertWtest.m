92df821d0816ecc40b10161bdf2a7d2f59847a01
Bijan Nemati <bijan.nemati@uah.edu>
2020-03-16 21:56:10 -0500

    Update lambertWtest.m
    randemgain3 now mostly works

--- emccd_detect_m/lambertWtest.m
+++ emccd_detect_m/lambertWtest.m
@@ -1,9 +1,10 @@
 close all
-clear 
+clear
 clc
+format compact
 p2 = @(x,g) (x * exp(-x/g)/g^2)
 c2 = @(x,g) (1-(1+x/g)*exp(-x/g))
-g2 = @(x,g) (g*lambertW(-1, (x-1)/exp(1)) - g)
+g2 = @(x,g) (-g*lambertw(-1, (x-1)/exp(1)) - g)
 
 g = 100
 
@@ -13,5 +14,12 @@ p2(x, g)
 
 c2(x, g)
 
-g2(c2(x, g), g)
+xset = 0:10:1000
+test = []
+for x = xset
+    test = [test, g2(c2(x, g), g)];
+end
 
+figure,
+plot(xset, test, 'o-')
+grid
