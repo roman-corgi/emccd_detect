fdeaf380c8e91e483b4038e93a637d7fdc782a7a
samhmiller <millerhsam@gmail.com>
2020-03-24 11:10:53 -0400

    Merge test_rand_em_gain_BN -> test_rand_em_gain

--- scripts_m/test_rand_em_gain.m
+++ scripts_m/test_rand_em_gain.m
@@ -1,4 +1,4 @@
-% Test rand_em_gain
+% Test rand_em_gain_new
 clear; close all; clc; format compact;
 addpath('../emccd_detect_m');
 addpath('../emccd_detect_m/util');
@@ -7,12 +7,17 @@ scrSize = get(0, 'MonitorPositions'); [nMon,~]=size(scrSize); iMon = min(jMon, n
 fsz = 400*[1.2,1];
 nr = round(scrSize(iMon,4)/fsz(1)); nc = round(scrSize(iMon,3)/fsz(2)); clear('jMon', 'nMon','fsz');
 
+eps_th1 = @(x,g) exp(-x/g);
+eps_th2 = @(x,g) (1+x/g).*exp(-x/g);
+eps_th3 = @(x,g) (1+(x/g)+0.5*(x/g)^2).*exp(-x/g);
+
+
 EMgain = 1000;
 
-% Verify that rand_em_gaincreates exactly the same array as
-% rand_em_gain_old_w for n_in greater than 2
-OnesMtx = ones(10);
-Narray = 2:3;
+% Verify that rand_em_gain_new creates exactly the same array as
+% rand_em_gain_w for average rates greater than 1
+OnesMtx = ones(1000);
+Narray = 1:2;
 for N = Narray
     NinMtx = OnesMtx * N;
     avg_rate = sum(NinMtx(:)) / numel(NinMtx);
@@ -24,7 +29,7 @@ fprintf('\n');
 
 % Check means
 Narray = 1:4;
-fprintf('%s %10s %10s\n', 'N', 'old_mean', 'new_mean');
+fprintf('%s   %s   %s\n', 'N', 'old_mean', 'new_mean');
 for N = Narray
     NinMtx = OnesMtx * N;
     
@@ -47,15 +52,24 @@ fprintf('\n')
 
 % Check threshold efficiency
 Narray = 1:2;
-thresh_array = 5:5:25;  % Percentage of EMgain
+thresh_array = 5:5:35;  % Percentage of EMgain
 for N = Narray
-    fprintf('N: %d\n', N);
-    fprintf(' %7s %7s %10s %10s\n', 'thr/g(%)', 'e', 'old e', 'new e'); 
+    fprintf('\nN: %d       threshold efficiencies, %% \n', N);
+    fprintf(' %7s  %8s %9s  %9s\n', 'thr/g(%)', 'expected', 'old gen', 'new gen'); 
     NinMtx = OnesMtx * N;
     i = 1;
     for percent = thresh_array
         thresh = percent/100 * EMgain;
-        e_pc(i) = exp(-thresh/EMgain);
+        switch N
+            case 1
+                e_pc(i) = eps_th1(thresh, EMgain); %#ok<*SAGROW>
+            case 2
+                e_pc(i) = eps_th2(thresh, EMgain);
+            case 3
+                e_pc(i) = eps_th3(thresh, EMgain);
+            otherwise
+                error('not handled');
+        end
 
         [old, new] = both_em_gain(NinMtx, EMgain);
 
@@ -67,7 +81,7 @@ for N = Narray
         pc_new(new > thresh) = 1;
         e_pc_new(i) = calc_efficiency(NinMtx, pc_new);
         
-        fprintf('%6d %10.3f %10.3f %10.3f\n', percent, e_pc(i), e_pc_old(i), e_pc_new(i));
+        fprintf('%6d %10.2f %10.2f %10.2f\n', percent, e_pc(i)*100, e_pc_old(i)*100, e_pc_new(i)*100);
         i = i + 1;
     end
     figure;
@@ -75,9 +89,9 @@ for N = Narray
     plot(thresh_array, e_pc_old);
     plot(thresh_array, e_pc_new);
     title([sprintf('N = %d\n', N), sprintf('PC Efficiencey')]);
-    xlabel('t/g (%)');
+    xlabel('threshold / gain  ,  %');
     ylabel('Efficiency');
-    legend('e', 'old e', 'new e');grid;
+    legend('expected', 'old gen', 'new gen');grid;
 end
 
 autoArrangeFigures(nr, nc, iMon); return;
@@ -89,9 +103,9 @@ function [old, new] = both_em_gain(NinMtx, EMgain)
 seed = 1;
 
 rng(seed);
-old = rand_em_gain_old_w(NinMtx, EMgain);
+old = rand_em_gain_w(NinMtx, EMgain);
 rng(seed);
-new = rand_em_gain(NinMtx, EMgain);
+new = rand_em_gain_new(NinMtx, EMgain);
 end
 
 
