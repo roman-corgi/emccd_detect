7231ac4612e6d6e56b27c7a578de9d9bdc87676d
samhmiller <millerhsam@gmail.com>
2020-04-30 16:44:27 -0400

    Update multi_frame.py

--- multi_frame.py
+++ multi_frame.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import, division, print_function
 from os import path
 from pathlib import Path
 
+import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
@@ -20,33 +21,50 @@ current_path = Path(path.dirname(__file__))
 fits_path = Path(current_path, 'data', fits_name)
 fluxmap = fits.getdata(fits_path)  # Input fluxmap (photons/pix/s)
 
-# Simulation inputs
-frametime = 100.  # Frame time (s)
-em_gain = 5000.  # CCD EM gain (e-/photon)
-full_well_image = 50000.  # Image area full well capacity (e-)
-full_well_serial = 90000.  # Serial (gain) register full well capacity (e-)
-dark_current = 0.0028  # Dark current rate (e-/pix/s)
-cic = 0.01  # Clock induced charge (e-/pix/frame)
-read_noise = 100.  # Read noise (e-/pix/frame)
-bias = 0.  # Bias offset (e-)
-qe = 0.9  # Quantum efficiency
-cr_rate = 1.  # Cosmic ray rate (5 for L2) (hits/cm^2/s)
-pixel_pitch = 13e-6  # Distance between pixel centers (m)
-shot_noise_on = True  # Apply shot noise
-
+# Number of images to create
 nfiles = 3
+
+# Plot output images
+plot_images = True
+
+# Make brights
 path = '/Users/sammiller/Documents/GitHub/proc_cgi_frame/data/sim/brights/'
 file_name = 'sim'
-plot_images = False
 for i in range(nfiles):
-    sim_im = emccd_detect(fluxmap, frametime, em_gain, full_well_image,
-                          full_well_serial, dark_current, cic, read_noise, bias,
-                          qe, cr_rate, pixel_pitch, shot_noise_on)
+    sim_im = emccd_detect(fluxmap,
+                          frametime=100.,
+                          em_gain=5000.,
+                          full_well_image=20000.,
+                          full_well_serial=48440.,
+                          dark_current=0.0028,
+                          cic=0.01,
+                          read_noise=100.,
+                          bias=5000.,
+                          qe=0.9,
+                          cr_rate=1.
+                          )
     fits.writeto(Path(path, '{}{}.fits'.format(file_name, i)), sim_im)
-
     if plot_images:
-        subtitle = ('Gain: {:.0f}   Read Noise: {:.0f}e-   Frame Time: {:.0f}s'
-                    .format(em_gain, read_noise, frametime))
-        imagesc(sim_im, 'Output Image\n' + subtitle)
+        imagesc(sim_im, 'Output Image')
+
+# Make darks
+path = '/Users/sammiller/Documents/GitHub/proc_cgi_frame/data/sim/darks/'
+file_name = 'sim'
+for i in range(nfiles):
+    sim_im = emccd_detect(np.zeros(fluxmap.shape),
+                          frametime=100.,
+                          em_gain=5000.,
+                          full_well_image=20000.,
+                          full_well_serial=48440.,
+                          dark_current=0.0028,
+                          cic=0.01,
+                          read_noise=100.,
+                          bias=5000.,
+                          qe=0.9,
+                          cr_rate=0.
+                          )
+    fits.writeto(Path(path, '{}{}.fits'.format(file_name, i)), sim_im)
+    if plot_images:
+        imagesc(sim_im, 'Output Image')
 
 plt.show()
