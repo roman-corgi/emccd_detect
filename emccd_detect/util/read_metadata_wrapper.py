fe6808da89ddfc1befc83290699839bbafeaec4e
samhmiller <millerhsam@gmail.com>
2020-10-26 16:55:31 -0500

    Create read_metadata_wrapper.py

---
+++ emccd_detect/util/read_metadata_wrapper.py
@@ -0,0 +1,75 @@
+"""Wrapper for read_metadata to allow use in emccd_detect simulator."""
+import numpy as np
+
+from emccd_detect.util.read_metadata import Metadata
+
+
+class ReadMetadataWrapprException(Exception):
+    """Exception class for read_metadata_wrapper module."""
+
+
+class MetadataWrapper(Metadata):
+    """Wrapper for Metadata class to add functionality for emccd_detect.
+
+    Parameters
+    ----------
+    meta_path : str
+        Full path of metadta yaml.
+
+    Attributes
+    ----------
+    data : dict
+        Data from metadata file.
+    geom : SimpleNamespace
+        Geometry specific data.
+    eperdn : float
+        Electrons per dn conversion factor (detector k gain).
+    fwc : float
+        Full well capacity of detector.
+    sat_thresh : float
+        Multiplication factor for fwc that determines saturated cosmic pixels.
+    plat_thresh : float
+        Multiplication factor for fwc that determines edges of cosmic plateu.
+    cosm_filter : int
+        Minimum length in pixels of cosmic plateus to be identified.
+    tail_filter : int
+        Moving median filter window size for cosmic tail subtraction.
+    cic_thresh : float
+        Multiplication factor for readnoise that determines beginning of cic.
+
+    B Nemati and S Miller - UAH - 03-Aug-2018
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.full_frame_zeros = np.zeros((self.frame_rows, self.frame_cols),
+                                         dtype=int)
+
+    def mask(self, key):
+        full_frame_m = self.full_frame_zeros.copy()
+
+        rows, cols, ul = self._unpack_geom(key)
+        full_frame_m[ul[0]:ul[0]+rows, ul[1]:ul[1]+cols] = 1
+        return full_frame_m.astype(bool)
+
+    def embed(self, key, data):
+        full_frame = self.full_frame_zeros.copy()
+
+        rows, cols, ul = self._unpack_geom(key)
+        try:
+            full_frame[ul[0]:ul[0]+rows, ul[1]:ul[1]+cols] = data
+        except Exception:
+            raise ReadMetadataWrapprException('Data does not fit in selected '
+                                              'section')
+        return full_frame
+
+    def active(self, full_frame):
+        """Select only the real counts from full frame and exclude virtual.
+
+        Use this to transform mask and embed from acting on the full frame to
+        acting on only the image frame.
+        """
+        rows_pre, cols_pre, ul_pre = self._unpack_geom('prescan')
+        rows_ovr, cols_ovr, ul_ovr = self._unpack_geom('overscan')
+        return full_frame[0:ul_ovr[0], ul_pre[1]+cols_pre:]
