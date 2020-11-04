2a964a8fbbc1280f27d01aa553c983b55ac826ba
samhmiller <millerhsam@gmail.com>
2020-11-03 19:19:30 -0500

    Move emccd_detect_new to emccd_detect

--- emccd_detect/emccd_detect.py
+++ emccd_detect/emccd_detect.py
@@ -11,9 +11,110 @@ from pathlib import Path
 from emccd_detect.cosmics import cosmic_hits, sat_tails
 from emccd_detect.rand_em_gain import rand_em_gain
 from emccd_detect.util.read_metadata import Metadata
+from emccd_detect.util.read_metadata_wrapper import MetadataWrapper
 
 here = Path(os.path.abspath(os.path.dirname(__file__)))
 META = Metadata(Path(here.parent, 'data', 'metadata.yaml'))
+
+
+class EMCCDDetect:
+    def __init__(self,
+                 frametime,
+                 em_gain=5000.,
+                 full_well_image=50000.,
+                 full_well_serial=90000.,
+                 dark_current=0.0028,
+                 cic=0.01,
+                 read_noise=100,
+                 bias=0.,
+                 qe=0.9,
+                 cr_rate=0.,
+                 pixel_pitch=13e-6,
+                 shot_noise_on=True,
+                 meta_path=None
+                 ):
+        self.frametime = frametime
+        self.em_gain = em_gain
+        self.full_well_image = full_well_image
+        self.full_well_serial = full_well_serial
+        self.dark_current = dark_current
+        self.cic = cic
+        self.read_noise = read_noise
+        self.bias = bias
+        self.qe = qe
+        self.cr_rate = cr_rate
+        self.pixel_pitch = pixel_pitch
+        self.shot_noise_on = shot_noise_on
+        self.meta_path = meta_path
+
+        # Initialize metadata
+        self.meta = MetadataWrapper(self.meta_path)
+
+    def sim_frame(self, fluxmap):
+        # Embed fluxmap in the correct position within the full frame
+        full_frame = self.meta.embed(self.meta.full_frame_zeros, 'image',
+                                     fluxmap)
+
+        # Get just the image counts
+        image_frame = self.image_section(self.meta.imaging_slice(full_frame))
+
+
+        full_frame = self.meta.imaging_embed(full_frame, image_frame)
+        serial_frame = self.serial_register(full_frame)
+
+        # Reshape from 1d to 2d
+        return serial_frame.reshape(full_frame.shape)
+
+    def image_section(self, active_frame):
+        # Mean photo-electrons after inegrating over frametime
+        mean_phe_map = active_frame * self.frametime * self.qe
+
+        # Mean expected rate after integrating over frametime
+        mean_dark = self.dark_current * self.frametime
+        mean_noise = mean_dark + self.cic
+
+        # Actualize electrons at the pixels
+        if self.shot_noise_on:
+            active_frame = np.random.poisson(mean_phe_map + mean_noise).astype(float)
+        else:
+            active_frame = mean_phe_map + np.random.poisson(mean_noise,
+                                                            mean_phe_map.shape
+                                                            ).astype(float)
+
+        # Simulate cosmic hits on image area
+        image = self.meta.slice_section_im(active_frame, 'image')
+        image_cosm = cosmic_hits(image, self.cr_rate, self.frametime,
+                                 self.pixel_pitch, self.full_well_image)
+
+        active_frame = self.meta.embed_im(active_frame, 'image', image_cosm)
+
+        # Cap at serial full well capacity
+        active_frame[active_frame > self.full_well_image] = self.full_well_image
+
+        return active_frame
+
+    def serial_register(self, full_frame):
+        # Actualize cic electrons in prescan and overscan pixels
+        virtual_mask = self.meta.mask('prescan') + self.meta.mask('overscan')
+        full_frame[virtual_mask] = np.random.poisson(full_frame[virtual_mask]
+                                                     + self.cic)
+
+        # Flatten image area row by row to simulate readout to serial register
+        serial_frame = full_frame.ravel()
+
+        # Apply EM gain
+        serial_frame = rand_em_gain(serial_frame, self.em_gain)
+
+        # Simulate saturation tails
+        # serial_frame = sat_tails(serial_frame, full_well_serial)
+        # Cap at full well capacity of gain register
+        serial_frame[serial_frame > self.full_well_serial] = self.full_well_serial
+
+        # Apply fixed pattern, read noise, and bias
+        serial_frame += make_fixed_pattern(serial_frame)
+        serial_frame += make_read_noise(serial_frame, self.read_noise) + self.bias
+
+        return serial_frame
 
 
 def emccd_detect(fluxmap,
@@ -85,13 +186,13 @@ def emccd_detect(fluxmap,
     return serial_frame
 
 
-def image_section(fluxmap, frametime, full_well_image, dark_current, cic, qe,
+def image_section(active_frame, frametime, full_well_image, dark_current, cic, qe,
                   cr_rate, pixel_pitch, shot_noise_on):
     """Simulate detector image section.
 
     Parameters
     ----------
-    fluxmap : array_like, float
+    active_frame : array_like, float
         Input fluxmap (photons/pix/s).
     frametime : float
         Frame time (s).
@@ -116,11 +217,6 @@ def image_section(fluxmap, frametime, full_well_image, dark_current, cic, qe,
         Image area frame (e-).
 
     """
-    ul = (0, 0)
-    active_frame = np.zeros([META.geom['image']['rows'],
-                             META.geom['image']['cols']])
-    active_frame = embed_fluxmap(fluxmap, active_frame, ul)
-
     # Mean photo-electrons after inegrating over frametime
     mean_phe_map = active_frame * frametime * qe
 
