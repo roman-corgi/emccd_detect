f54a8a6964d9a5a65011b450848d677a93c3bbb8
samhmiller <millerhsam@gmail.com>
2020-11-16 16:14:17 -0600

    Update traps_script.py

--- traps_script.py
+++ traps_script.py
@@ -10,10 +10,6 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 
 from emccd_detect.emccd_detect import EMCCDDetect
-from emccd_detect.util.imagesc import imagesc
-from arcticpy.roe import ROE
-from arcticpy.ccd import CCD
-from arcticpy.traps import Trap
 from arcticpy.main import model_for_HST_ACS
 
 
@@ -32,33 +28,33 @@ def imagesc(data, title=None, vmin=None, vmax=None, cmap='viridis',
 
 
 if __name__ == '__main__':
-        here = os.path.abspath(os.path.dirname(__file__))
+    here = os.path.abspath(os.path.dirname(__file__))
 
-        # Input fluxmap
-        fits_name = 'sci_frame.fits'
-        fits_path = Path(here, 'data', fits_name)
-        fluxmap = fits.getdata(fits_path).astype(float)  # Input fluxmap (photons/pix/s)
+    # Input fluxmap
+    fits_name = 'sci_frame.fits'
+    fits_path = Path(here, 'data', fits_name)
+    fluxmap = fits.getdata(fits_path).astype(float)  # Input fluxmap (photons/pix/s)
 
-        # Simulation inputs
-        frametime = 100.  # Frame time (s)
-        em_gain = 5000.  # CCD EM gain (e-/photon)
-        full_well_image = 50000.  # Image area full well capacity (e-)
-        full_well_serial = 90000.  # Serial (gain) register full well capacity (e-)
-        dark_current = 0.0028  # Dark current rate (e-/pix/s)
-        cic = 0.01  # Clock induced charge (e-/pix/frame)
-        read_noise = 100.  # Read noise (e-/pix/frame)
-        bias = 0.  # Bias offset (e-)
-        qe = 0.9  # Quantum efficiency
-        cr_rate = 0.  # Cosmic ray rate (5 for L2) (hits/cm^2/s)
-        pixel_pitch = 13e-6  # Distance between pixel centers (m)
-        shot_noise_on = True  # Apply shot noise
+    # Simulation inputs
+    frametime = 100.  # Frame time (s)
+    em_gain = 5000.  # CCD EM gain (e-/photon)
+    full_well_image = 50000.  # Image area full well capacity (e-)
+    full_well_serial = 90000.  # Serial (gain) register full well capacity (e-)
+    dark_current = 0.0028  # Dark current rate (e-/pix/s)
+    cic = 0.01  # Clock induced charge (e-/pix/frame)
+    read_noise = 100.  # Read noise (e-/pix/frame)
+    bias = 0.  # Bias offset (e-)
+    qe = 0.9  # Quantum efficiency
+    cr_rate = 0.  # Cosmic ray rate (5 for L2) (hits/cm^2/s)
+    pixel_pitch = 13e-6  # Distance between pixel centers (m)
+    shot_noise_on = True  # Apply shot noise
 
-        # Traps at Hubble launch date
-        date = 2452334.5
-        traps, ccd, roe = model_for_HST_ACS(date)
+    # Traps at Hubble launch date
+    date = 2452334.5
+    traps, ccd, roe = model_for_HST_ACS(date)
 
-        # Instantiate class
-        emccd = EMCCDDetect(
+    # Instantiate class
+    emccd = EMCCDDetect(
         meta_path=Path(here, 'data', 'metadata.yaml'),
         em_gain=em_gain,
         full_well_image=full_well_image,
@@ -71,21 +67,21 @@ if __name__ == '__main__':
         cr_rate=cr_rate,
         pixel_pitch=pixel_pitch,
         shot_noise_on=shot_noise_on
-        )
+    )
 
-        # No traps
-        sim_frame_notrap = emccd.sim_sub_frame(fluxmap, frametime)
+    # No traps
+    sim_frame_notrap = emccd.sim_sub_frame(fluxmap, frametime)
 
-        # Expected rate for given run
-        expected_rate = emccd.mean_expected_rate
+    # Expected rate for given run
+    expected_rate = emccd.mean_expected_rate
 
-        # Traps
-        emccd.update_cti(ccd=ccd, roe=roe, traps=traps, express=1)
-        sim_frame_trap = emccd.sim_sub_frame(fluxmap, frametime)
+    # Traps
+    emccd.update_cti(ccd=ccd, roe=roe, traps=traps, express=1)
+    sim_frame_trap = emccd.sim_sub_frame(fluxmap, frametime)
 
-        # Plot images
-        plot_images = True
-        if plot_images:
+    # Plot images
+    plot_images = True
+    if plot_images:
         imagesc(fluxmap, 'Input Fluxmap (phot/pix/s)')
 
         vmin = np.min(expected_rate)
