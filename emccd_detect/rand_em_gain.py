8d3894790f9fae5d11ce2ed197a9c45e491eb0b7
samhmiller <millerhsam@gmail.com>
2020-12-01 16:56:39 -0600

    Major improvements to rand_em_gain

--- emccd_detect/rand_em_gain.py
+++ emccd_detect/rand_em_gain.py
@@ -10,7 +10,7 @@ class RandEMGainException(Exception):
     """Exception class for rand_em_gain module."""
 
 
-def rand_em_gain(n_in_array, em_gain):
+def rand_em_gain(n_in_array, em_gain, max_out):
     """Generate random numbers according to EM gain pdfs.
 
     Parameters
@@ -19,11 +19,13 @@ def rand_em_gain(n_in_array, em_gain):
         Array of electron values (e-).
     em_gain : float
         CCD em_gain (e-/photon).
+    max_out : float
+        Maximum allowed output, used to set a end bound on distributions (e-).
 
     Returns
     -------
-    out : array_like
-        Electron values multiplied by random EM gain distribution.
+    array_like
+        Electron values multiplied by random EM gain distribution (e-).
 
     Notes
     -----
@@ -43,70 +45,47 @@ def rand_em_gain(n_in_array, em_gain):
     if em_gain < 1:
         raise RandEMGainException('EM gain cannot be set to less than 1')
 
-    # Find how many values in a array are equal to 0, 1, 2, or >= 3
-    y = np.zeros(n_in_array.size)
-    inds0 = (n_in_array == 0).nonzero()[0]
-    inds1 = (n_in_array == 1).nonzero()[0]
-    inds2 = (n_in_array == 2).nonzero()[0]
-    inds3 = (n_in_array > 2).nonzero()[0]
+    # Initialize output count array
+    n_out_array = np.zeros_like(n_in_array)
 
-    # For n_in of 0, 1, or 2, generate arrays of random numbers according to gain
-    # equations specific to each n_in
-    n0 = len(inds0)
-    n1 = len(inds1)
-    n2 = len(inds2)
-    y[inds0] = _rand_em_exact(0, n0, em_gain)
-    y[inds1] = _rand_em_exact(1, n1, em_gain)
-    y[inds2] = _rand_em_exact(2, n2, em_gain)
+    # Get unique nonzero n_in values
+    n_in_unique = np.unique(n_in_array)
+    n_in_unique = n_in_unique[n_in_unique > 0]
 
-    # For n_in of 3 or greater, generate random numbers one by one according to the
-    # generalized gain equation
-    for i in inds3:
-        n_in = n_in_array[i]
-        y[i] = _rand_em_approx(n_in, em_gain)
+    for n_in in n_in_unique:
+        inds = np.where(n_in_array == n_in)[0]
+        n_out_array[inds] = _rand_em_exact(n_in, em_gain, max_out, len(inds))
 
-    return np.reshape(y, n_in_array.shape)
+    return n_out_array
 
 
-def _rand_em_exact(n_in, numel, g):
+def _rand_em_exact(n_in, em_gain, x_max, numel):
     """Select a gain distribution based on n_in and generate random numbers."""
     x = np.random.random(numel)
 
-    if n_in == 0:
-        y = np.zeros(numel)
-    elif n_in == 1:
-        y = -g * np.log(1 - x)
+    if n_in == 1:
+        n_out = -em_gain * np.log(1 - x)
     elif n_in == 2:
-        y = -g * special.lambertw((x-1)/np.exp(1), -1).real - g
+        n_out = -em_gain * special.lambertw((x-1)/np.exp(1), -1).real - em_gain
+    else:
+        x_axis = np.arange(0, x_max)
+        x_axis[0] = np.finfo(float).eps
 
-    return np.round(y)
+        cdf = _get_cdf(n_in, em_gain, x_axis)
+        cdf_lookups = (cdf.max() - cdf.min()) * x + cdf.min()
+        n_out = x_axis[np.searchsorted(cdf, cdf_lookups)]  # XXX This could be made more accurate
+
+    return np.round(n_out)
 
 
-def _rand_em_approx(n_in, g):
+def _get_cdf(n_in, em_gain, x):
     """Select gain distribution based on n_in and generate single random number."""
-    kmax = 5
-    xmin = np.finfo(float).eps
-    xmax = kmax * n_in * g
-    nx = 800
-    x = np.linspace(xmin, xmax, nx)
 
     # Basden 2003 probability distribution function is as follows:
     # pdf = x.^(n_in-1) .* exp(-x/g) / (g^n_in * factorial(n_in-1))
     # Because of the cancellation of very large numbers, first work in log space
-    logpdf = (n_in-1)*np.log(x) - x/g - n_in*np.log(g) - special.gammaln(n_in)
+    logpdf = (n_in-1)*np.log(x) - x/em_gain - n_in*np.log(em_gain) - special.gammaln(n_in)
     pdf = np.exp(logpdf)
     cdf = np.cumsum(pdf / np.sum(pdf))
 
-    # Create a uniformly distributed random number for lookup in CDF
-    cdf_lookup = np.random.uniform(min(cdf), max(cdf))
-
-    # Map random value to cdf
-    ihi = (cdf > cdf_lookup).nonzero()[0][0]
-    ilo = ihi - 1
-    xlo = x[ilo]
-    xhi = x[ihi]
-    clo = cdf[ilo]
-    chi = cdf[ihi]
-    y = xlo + (cdf_lookup - clo) * (xhi - xlo)/(chi-clo)
-
-    return np.round(y)
+    return cdf
