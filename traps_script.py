d8c29b2e79e4d6b03b906ca0aeda7c325229361e
samhmiller <millerhsam@gmail.com>
2020-12-11 18:25:43 -0500

    Clean up traps script

--- traps_script.py
+++ traps_script.py
@@ -46,9 +46,9 @@ if __name__ == '__main__':
         meta_path=meta_path,
         em_gain=5000.,
         full_well_image=60000,  # e-
-        dark_current=0.0028,  # e-/pix/s
-        cic=0.02,  # e-/pix/s
-        read_noise=100,  # e-
+        dark_current=0.00,  # e-/pix/s
+        cic=0.0,  # e-/pix/s
+        read_noise=0,  # e-
         bias=10000,  # e-
         qe=0.9,
         cr_rate=0.,  # hits/cm^2/s
@@ -59,34 +59,33 @@ if __name__ == '__main__':
         nbits=14
     )
 
-    # No traps
-    frametime = 100  # s
+    # Simulate without traps
+    frametime = 1  # s
     sim_frame_notrap = emccd.sim_sub_frame(fluxmap, frametime)
 
-    # Expected rate for given run
+    # Get expected rate for given run
     expected_rate = emccd.mean_expected_rate
 
-    # Traps
+    # Simulate with traps
     emccd.update_cti(ccd=ccd, roe=roe, traps=traps, express=1)
     sim_frame_trap = emccd.sim_sub_frame(fluxmap, frametime)
 
+
     # Plot images
-    plot_images = True
-    if plot_images:
-        imagesc(fluxmap, 'Input Fluxmap (phot/pix/s)')
+    imagesc(fluxmap, 'Input Fluxmap (phot/pix/s)')
 
-        vmin = np.min(expected_rate)
-        vmax = np.max(expected_rate)
+    vmin = np.min(expected_rate)
+    vmax = np.max(expected_rate)
 
-        # Convert output arrays from dn to electrons
-        notrap = (sim_frame_notrap*emccd.eperdn - emccd.bias) / emccd.em_gain
-        trap = (sim_frame_trap*emccd.eperdn - emccd.bias) / emccd.em_gain
+    # Convert output arrays from dn to electrons
+    notrap = (sim_frame_notrap*emccd.eperdn - emccd.bias) / emccd.em_gain
+    trap = (sim_frame_trap*emccd.eperdn - emccd.bias) / emccd.em_gain
 
-        subtitle = f'Frametime = {int(frametime)}s'
-        imagesc(expected_rate, 'Mean Expected Rate (e-/pix)\n' + subtitle)
-        imagesc(notrap, 'Output Without Traps (e-/pix)\n' + subtitle,
-                vmin=vmin, vmax=vmax)
-        imagesc(trap, 'Output With Traps (e-/pix)\n' + subtitle,
-                vmin=vmin, vmax=vmax)
+    subtitle = f'Frametime = {int(frametime)}s'
+    imagesc(expected_rate, 'Mean Expected Rate (e-/pix)\n' + subtitle)
+    imagesc(notrap, 'Output Without Traps (e-/pix)\n' + subtitle,
+            vmin=vmin, vmax=vmax)
+    imagesc(trap, 'Output With Traps (e-/pix)\n' + subtitle,
+            vmin=vmin, vmax=vmax)
 
-        plt.show()
+    plt.show()
