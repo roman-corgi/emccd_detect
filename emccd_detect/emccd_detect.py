4d940fa8c9b4d8883af0247b982e8834e98b62fe
Neil Zimmerman <neiltzim@gmail.com>
2022-11-22 10:37:37 -0500

    import modified arctic trap class

--- emccd_detect/emccd_detect.py
+++ emccd_detect/emccd_detect.py
@@ -10,7 +10,8 @@ import numpy as np
 from emccd_detect.cosmics import cosmic_hits, sat_tails
 from emccd_detect.rand_em_gain import rand_em_gain
 from emccd_detect.util.read_metadata_wrapper import MetadataWrapper
-from arcticpy import add_cti, CCD, ROE, Trap
+from arcticpy import add_cti, CCD, ROE
+from arcticpy import TrapInstantCapture as Trap
 
 
 class EMCCDDetectException(Exception):
